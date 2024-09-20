9d859c9e7a3b9c1f1a7f5106a239c1d3597563c4
Ronal Fabra <rfabra@ext.laligatech.com>
2024-09-20 07:53:14 -0500

    added new script to run the unit tests with github actions

---
+++ run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCHEME='InstaflixTests'
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
+
+xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
